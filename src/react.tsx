d5b54f0f387c2df1fa8efae7cb46d6e51aa1e95a
takagimeow <dev@takagi.app>
2021-01-14 19:11:24 +0900

    ✨ ESLintの導入

--- src/react.tsx
+++ src/react.tsx
@@ -1,9 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 // src/react.tsx
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-const Index = () => {
-	return <div>Hello React!</div>;
-};
+const Index = () => <div>Hello React!</div>;
 
-ReactDOM.render(<Index />, document.getElementById("app"));
+ReactDOM.render(<Index />, document.getElementById('app'));
